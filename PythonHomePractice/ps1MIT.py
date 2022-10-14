81dea7ee65340c62937763ea91aceae4c919485c
Jumman55 <jumman.1948848@studenti.uniroma1.it>
2022-10-14 18:27:34 +0200

    TyepeError: can only concatenate str (not int) to str , resolved

--- PythonHomePractice/ps1MIT.py
+++ PythonHomePractice/ps1MIT.py
@@ -28,3 +28,30 @@ def numberOfMonths():
         current_savings += ((monthly_salary * portion_saved) + (current_savings * (r/12)))
         
     return number_of_months
+
+
+def numberOfMonthsWithRaise():
+    """How many months it will take to save up enough money for a dwon payment with half yearly raise"""
+    
+    #user interaction 
+    annual_salary = float(input("Enter your annual salary: "))
+    portion_saved = float(input("Enter the percent of your salary to save, as decimal: "))
+    total_cost = float(input("Enter the cost of your dream home: "))
+    semi_annual_raise = float(input("Enter the semi-annual raise, as decimal: "))
+    
+    #simple calculation
+    portion_down_payment = total_cost * 0.25
+    current_savings = 0
+    monthly_salary = annual_salary / 12
+    number_of_months = 0
+    # counter = 0
+    r = 0.04
+    
+    #TODO when total month is odd number it return 1 less value ??
+    while portion_down_payment >= current_savings:
+        number_of_months += 1
+        if number_of_months % 6 == 0:
+            monthly_salary += (monthly_salary * semi_annual_raise)
+        current_savings += ((monthly_salary * portion_saved) + (current_savings * (r/12)))
+        
+    return number_of_months
